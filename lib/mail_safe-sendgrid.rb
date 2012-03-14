40727c131b6be5af464cc2626cd163bfa7ae41ed
Ed Shadi and Vijay Ramesh <tech_ops+ed+vijay@change.org>
2012-03-13 17:19:51 -0700

    handle the case where a single email is being sent without any additional addresses in the X_SMTPAPI headers

--- lib/mail_safe-sendgrid.rb
+++ lib/mail_safe-sendgrid.rb
@@ -10,11 +10,12 @@ module MailSafe
         # we don't want to call add_body_postscript until after we've added the SendGrid SMTPAPI
         # replaced addresses to the list of all addresses replaced by Mail Safe
         @@replaced_addresses.reverse_merge! replaced_addresses
+        add_xsmtpapi_to_address_types if replaced_addresses[:X_SMTPAPI]
         if call_super or replaced_addresses[:X_SMTPAPI]
-          add_xsmtpapi_to_address_types
           add_body_postscript_without_mail_header(part, @@replaced_addresses)
-          remove_xsmtpapi_from_address_types
         end
+        remove_xsmtpapi_from_address_types if replaced_addresses[:X_SMTPAPI]
+
       end
       alias_method_chain :add_body_postscript, :mail_header
 
@@ -26,26 +27,30 @@ module MailSafe
 
         external_indices = []
         # get a list of indices for external addresses
-        x_smtpapi['to'].each_with_index do |address, index|
-          external_indices << index if !MailSafe::Config.is_internal_address?(address)
-        end
-        deleted_cnt = 0
-        deleted_addresses = []
-        external_indices.each do |i|
-          # remove any external address from :to
-          deleted_addresses << x_smtpapi['to'].delete_at(i - deleted_cnt)
-          # remove the related tokens for every array in :sub
-          x_smtpapi['sub'].each do |k, v|
-            v.delete_at(i-deleted_cnt)
-            x_smtpapi['sub'][k] = v
+        if x_smtpapi['to']
+          x_smtpapi['to'].each_with_index do |address, index|
+            external_indices << index if !MailSafe::Config.is_internal_address?(address)
           end
-          # the indices decrement as we remove elements, deleted_cnt accounts for this
-          deleted_cnt += 1
+          deleted_cnt = 0
+          deleted_addresses = []
+          external_indices.each do |i|
+            # remove any external address from :to
+            deleted_addresses << x_smtpapi['to'].delete_at(i - deleted_cnt)
+            # remove the related tokens for every array in :sub
+            x_smtpapi['sub'].each do |k, v|
+              v.delete_at(i-deleted_cnt)
+              x_smtpapi['sub'][k] = v
+            end
+            # the indices decrement as we remove elements, deleted_cnt accounts for this
+            deleted_cnt += 1
+          end
+          # save the headers back to X-SMTPAPI
+          mail.header['X-SMTPAPI'].value = x_smtpapi.to_json.gsub(/(["\]}])([,:])(["\[{])/, '\\1\\2 \\3')
+          add_body_postscript(mail,{:X_SMTPAPI=>deleted_addresses}, true)
+        else
+          add_body_postscript(mail,{}, true)
         end
-        # save the headers back to X-SMTPAPI
-        mail.header['X-SMTPAPI'].value = x_smtpapi.to_json.gsub(/(["\]}])([,:])(["\[{])/, '\\1\\2 \\3')
 
-        add_body_postscript(mail,{:X_SMTPAPI=>deleted_addresses}, true)
         # we need to override add_text_postscript to include sendgrid x-smtpapi and not ADDRESS_TYPES only
 
       end
